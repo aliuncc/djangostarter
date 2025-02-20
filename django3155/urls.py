0acc4bcbb31a074484121a5a218c696a9d7ebbbf
aliuncc <achught1@charlotte.edu>
2025-02-20 13:03:35 -0500

    screenshots added

--- django3155/urls.py
+++ django3155/urls.py
@@ -1,6 +1,8 @@
 #routes
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -12,3 +14,8 @@ urlpatterns = [
     path('', include('base.urls')),
     path('api/', include('base.api.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
